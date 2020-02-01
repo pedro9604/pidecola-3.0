f4268587bed74a57670ed455c7dcc61940473cfd
fmarquez199 <fjmarquez199@gmail.com>
2020-01-31 21:28:00 -0400

    Improved Pide Cola 3.0 msg

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController.js')
+const rideController = require('../controllers/rideController.js')
 
 // POSTs
 router.post('/', userController.create)
+router.post('/', rideController.create)
 
 module.exports = router
