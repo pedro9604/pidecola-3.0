d3a66df84d8a5ff362743a0f8ff17bf78f8c30c0
Pedro Maldonado <pjmg96@gmail.com>
2020-03-12 19:16:28 -0400

    Schema modularization
    Separated statistics fields from User Schema.
    Updated profile and vehicle pic fields

--- models/userModel.js
+++ models/userModel.js
@@ -17,7 +17,7 @@ const UserSchema = new Schema(
       'Ing. de Computación', 'Ing. Geofísica', 'Ing. de Producción', 'Ing. de Telecomunicaciones', 
       'Arquitectura', 'Urbanismo', 'Lic. en Química', 'Lic. en Matemáticas', 'Lic. en Física', 'Lic. en Biología', 
       'Lic. en Comercio Internacional', 'Lic. en Estudios y Artes Liberales', 'Economía']},
-    profile_pic: { type: Schema.Types.ObjectId, ref: 'Img' },
+    profile_pic: { type: String },
     status: { type: String, enum: ['Disponible', 'No Disponible']},
     community: { type: String, enum: ['Estudiante', 'Profesor', 'Egresado', 'Personal Administrativo', 'Obrero']},
     vehicles:
@@ -27,18 +27,9 @@ const UserSchema = new Schema(
           year: { type: Number },
           color: { type: String },
           vehicle_capacity: {type: Number},
-          vehicle_pic: [{ type: Schema.Types.ObjectId, ref: 'Img' }]
+          vehicle_pic: { type: String }
 
-        }],
-    license: { type: String },    
-    rides_given: { type: Number },
-    rides_recieved: { type: Number },
-    likes_count: { type: Number },
-    dislikes_count: { type: Number },
-    last_ride: { type: Schema.Types.ObjectId, ref: 'Ride'},
-    frequent_routes: { type: Array },
-
-
+        }],   
   }, {
     timestamps: true
   }
