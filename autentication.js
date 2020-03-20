04aa98b850d915c9028a3cd155eb57e2867a57b1
Angel <morante413@gmail.com>
2020-03-20 14:59:43 -0400

    resolve bug

--- autentication.js
+++ autentication.js
@@ -28,7 +28,7 @@ exports.generateToken = (email, expiresIn = '30d') => {
 exports.signIn = (req, res) => {
   const { email, password } = viewCredentials(req.headers.authorization)
   if (!email || !password) return res.status(401).send(response(false, null, 'Invalid Credentials.'))
-  users.findByEmail(email)
+  users.findByEmail(email, {})
     .then(async user => {
       if (!user) return undefined
       const compare = await bcrypt.compare(password, user.password)
@@ -38,7 +38,6 @@ exports.signIn = (req, res) => {
       if (!resp) return res.status(401).send(response(false, null, 'User not found.'))
       else if (!resp[0]) return res.status(401).send(response(false, null, 'Invalid Password.'))
       const user = resp[1]._doc
-      delete user.password
       res.status(200).send(response(true, [{ tkauth: this.generateToken(user.email), ...user }], 'Success.'))
     })
     .catch(error => {
