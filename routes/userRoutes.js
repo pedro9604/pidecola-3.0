6d24ab4e768879bd382c887dff90a6da42f21dd1
Pedro Maldonado <pjmg96@gmail.com>
2020-03-26 16:45:51 -0400

    Unauthroized register error fixed
    Arreglado error Unauthorized al tratar de registrarse.
    - Se cambio la ruta de update profile (Avisar a equipo de front) para permitir acceder sin autorizacion solo a crear usuario.
    - Validacion de correos solo usb.ve
    - Detalle en endpoint de crear usuario.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,7 @@ router.post('/', userController.create)
 router.post('/code', userController.codeValidate)
 
 // PUTs
-router.put('/', userController.updateUser)
+router.put('/update/profile', userController.updateUser)
 router.put('/update/picture', upload.single('file'), userController.updateProfilePic)
 router.put('/add/vehicle', upload.single('file'), userController.addVehicle)
 router.put('/delete/vehicle', userController.deleteVehicle)
