d6da68ac14a2eb0f24a73a9e6d7a692864694f7a
Pedro Maldonado <pjmg96@gmail.com>
2020-03-30 11:59:39 -0400

    added countRidesGiven function

--- controllers/statisticsController.js
+++ controllers/statisticsController.js
@@ -1,18 +1,23 @@
 const rides = require('../models/rideModel.js')
 
 
-const countLikes = (email) => {
+const countRidesGiven = (email) => {
+    return rides.countDocuments({ rider: email })
+}
+
+/* const countLikes = (email) => {
     return rides.countDocuments({ rider: email, comments: {like: 1} })
 }
 
 const countDislikes = (email) => {
     return rides.countDocuments({ rider: email, comments: {dislike: 1} })
-}
+} */
 
 exports.getStatistics = (req, res) => {
     const email = req.secret.email
     if (!email) return res.status(401).send(response(false, '', 'El Email es necesario.'))
-    console.log(countLikes(email))
-    console.log(countDislikes(email))
+    //console.log(countLikes(email))
+    //console.log(countDislikes(email))
+    console.log(countRidesGiven(email))
     res.status(200).send(response(true, null, 'Estadisticas'))
 }
