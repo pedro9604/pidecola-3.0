a1f0583d48223fd78031311715a1517eb6bbaa67
Pedro Maldonado <pjmg96@gmail.com>
2020-03-30 18:26:55 -0400

    implemented statistics endpoints
    Endpoints para
    - Obtener nro de likes
    - Obtener nro de dislikes
    - Obtener nro de colas dadas
    - Obtener nro de colas recibidas

--- controllers/statisticsController.js
+++ controllers/statisticsController.js
@@ -1,23 +1,47 @@
 const rides = require('../models/rideModel.js')
+const response = require('../lib/utils/response').response
 
-
-const countRidesGiven = (email) => {
-    return rides.countDocuments({ rider: email })
-}
-
-/* const countLikes = (email) => {
-    return rides.countDocuments({ rider: email, comments: {like: 1} })
-}
-
-const countDislikes = (email) => {
-    return rides.countDocuments({ rider: email, comments: {dislike: 1} })
-} */
-
-exports.getStatistics = (req, res) => {
+exports.getRidesGiven = (req, res) => {
     const email = req.secret.email
     if (!email) return res.status(401).send(response(false, '', 'El Email es necesario.'))
-    //console.log(countLikes(email))
-    //console.log(countDislikes(email))
-    console.log(countRidesGiven(email))
-    res.status(200).send(response(true, null, 'Estadisticas'))
+    rides.countDocuments({ rider: email }, function(err, result){
+        if (err) return res.status(400).send(response(false, err, 'Error en la consulta'))
+        else {
+            return res.status(200).send(response(true, result, 'Numero de Colas Dadas'))
+        }
+    })
 }
+
+exports.getRidesReceived = (req, res) => {
+    const email = req.secret.email
+    if (!email) return res.status(401).send(response(false, '', 'El Email es necesario.'))
+    rides.countDocuments({ passenger: email }, function(err, result){
+        if (err) return res.status(400).send(response(false, err, 'Error en la consulta'))
+        else {
+            return res.status(200).send(response(true, result, 'Numero de Colas Recibidas'))
+        }
+    })
+}
+
+exports.getLikesCount = (req, res) => {
+    const email = req.secret.email
+    if (!email) return res.status(401).send(response(false, '', 'El Email es necesario.'))
+    rides.countDocuments({ rider: email, "comments.like": true }, function(err, result){
+        if (err) return res.status(400).send(response(false, err, 'Error en la consulta'))
+        else {
+            return res.status(200).send(response(true, result, 'Numero de Likes Recibidos'))
+        }
+    })
+}
+
+exports.getDislikesCount = (req, res) => {
+    const email = req.secret.email
+    if (!email) return res.status(401).send(response(false, '', 'El Email es necesario.'))
+    rides.countDocuments({ rider: email, "comments.dislike": true }, function(err, result){
+        if (err) return res.status(400).send(response(false, err, 'Error en la consulta'))
+        else {
+            return res.status(200).send(response(true, result, 'Numero de Dislikes Recibidos'))
+        }
+    })
+}
+
--- routes/statisticsRoutes.js
+++ routes/statisticsRoutes.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const statisticsController = require('../controllers/statisticsController.js')
 
-router.get('/', statisticsController.getStatistics)
+router.get('/rides/given', statisticsController.getRidesGiven)
+router.get('/rides/received', statisticsController.getRidesReceived)
+router.get('/count/likes', statisticsController.getLikesCount)
+router.get('/count/dislikes', statisticsController.getDislikesCount)
 
 module.exports = router
